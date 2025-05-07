01e95f94c56fce5ae789c94ed0bce34179f6d31b
metin delibas <m.metiin7@gmail.com>
2025-05-07 12:29:07 +0300

    addMigrations

--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7ab44795559f75bf4364888eec3bd872e59417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7826c9131caa0dd99426c007605f144b27a390")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
