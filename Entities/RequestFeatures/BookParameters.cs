b78af3d36c4d4aba53d42fabee1af0f43095be48
mehmetdelibas <m.metiin7@gmail.com>
2025-05-27 15:23:09 +0300

    addSearc

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -1,10 +1,13 @@
 ﻿namespace Entities.RequestFeatures
 {
     public class BookParameters : RequestParameters
-	{
+    {
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000;
 
         public bool ValidPriceRange => MaxPrice > MinPrice;
+
+        public String? SearchTerm { get; set; }
     }
+    
 }
--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60156017fea83b62a93f552eb3ed90ebd0a1a9e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eedd31d343af9e275dbcc186caebb93be936647")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
